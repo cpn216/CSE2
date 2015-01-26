8ab02fec93044dbc9b5957cb2285730b47eccce6
cpn216 <cpn216@lehigh.edu>
2015-01-26 02:48:20 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//
+//Cuong Nguyen
+//
+//Welcome Class Homework Assignment
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  displays welcome on the screen
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-C--P--N--2--1--6->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("In a world with so much to do, my favorite");
+    System.out.println("pastime is to do nothing.");
+    
+    
+    }
+}
